ec5c0f0f0dc195b86fded4b273e8617180ddfd4e
HelliWrold <albert_yun@qq.com>
2023-04-25 22:49:52 +0800

    已经完成规则组件，等待规则保存以及发布

--- src/views/rules/rules.vue
+++ src/views/rules/rules.vue
@@ -1,24 +1,438 @@
 <template>
   <div>
-    <div class="rule-op-container">
+    <div class="rules-op-container">
       <el-button class="el-button-append" @click="" type="primary">追加规则</el-button>
       <el-button class="el-button-reset" @click="" type="danger">重建规则</el-button>
       <el-button class="el-button-publish" @click="" type="success">规则发布</el-button>
     </div>
 
-    <div class="json-edit-container"></div>
+    <div class="rule-editor-container">
+      <div class="json-edit-container">
+        <vue-json-editor class="json-editor" v-model="rules" :showBtns="false" :mode="'code'" @json-change="onJsonChange"
+          @json-save="onJsonSave" @has-error="onError">
+        </vue-json-editor>
+      </div>
+
+      <div class="rule-op-container">
+        <div class="rule-source-container">
+          <el-input class="el-input-source" type="text" placeholder="输入数据源DevAddr" v-model="source" clearable
+            maxlength="8" show-word-limit>
+          </el-input>
+          <el-button class="el-button-source" @click="confirmSource" type="primary">确认</el-button>
+        </div>
+        <div class="rule-targets-container">
+          <el-input class="el-input-targets" placeholder="输入控制目标DevAddr" v-model="target" clearable>
+          </el-input>
+          <el-button class="el-button-targets" @click="appendTarget" type="primary">追加</el-button>
+        </div>
+
+        <div class="rule-condition-container">
+          <div class="condition-co">
+            <span class="slider-text">CO({{ co[0] + ':' + co[1] }})</span>
+            <el-slider v-model="co" range show-stops :min="10" :max="1000" show-input-controls>
+            </el-slider>
+          </div>
+          <div class="condition-co2">
+            <span class="slider-text">CO2({{ co2[0] + ':' + co2[1] }})</span>
+            <el-slider v-model="co2" range show-stops :min="400" :max="60000" :step="4">
+            </el-slider>
+          </div>
+          <div class="condition-h2s">
+            <span class="slider-text">H2S({{ h2s[0] + ':' + h2s[1] }})</span>
+            <el-slider v-model="h2s" range show-stops :min="1" :max="200">
+            </el-slider>
+          </div>
+          <div class="condition-lux">
+            <span class="slider-text">Light({{ lux[0] + ':' + lux[1] }})</span>
+            <el-slider v-model="lux" range show-stops :min="0" :max="65535" :step="5">
+            </el-slider>
+          </div>
+          <div class="condition-nh3">
+            <span class="slider-text">NH3({{ nh3[0] + ':' + nh3[1] }})</span>
+            <el-slider v-model="nh3" range show-stops :min="5" :max="500">
+            </el-slider>
+          </div>
+          <div class="condition-humi">
+            <span class="slider-text">HUMI({{ humi[0] + ':' + humi[1] }})</span>
+            <el-slider v-model="humi" range show-stops :min="20" :max="90" :step="0.1">
+            </el-slider>
+          </div>
+          <div class="condition-temp">
+            <span class="slider-text">TEMP({{ temp[0] + ':' + temp[1] }})</span>
+            <el-slider v-model="temp" range show-stops :min="0" :max="50" :step="0.1">
+            </el-slider>
+          </div>
+          <div class="condition-append-container">
+            <el-button class="el-button-append" @click="appendCondition" type="primary">追加条件组</el-button>
+          </div>
+        </div>
+
+        <div class="rule-actions-container">
+          <span class="switch-text">风扇</span>
+          <el-switch v-model="fun" active-text="打开" inactive-text="关闭">
+          </el-switch>
+          <span class="switch-text">灯</span>
+          <el-switch v-model="light" active-text="打开" inactive-text="关闭">
+          </el-switch>
+          <el-button class="el-button-source" @click="appendAction" type="primary">追加动作组</el-button>
+        </div>
+
+      </div>
+
+
+    </div>
+
   </div>
 </template>
 
+<script>
+// 导入模块
+import app from '@/store/modules/app'
+import vueJsonEditor from 'vue-json-editor'
+
+export default {
+  // 注册组件
+  components: { vueJsonEditor },
+  data() {
+    return {
+      rules: {
+        "0": {
+          "source": "3EB4A376",
+          "conditions": {
+            "0": {
+              "co2min": 0,
+              "co2max": 500,
+              "comin": 0,
+              "comax": 200,
+              "h2smin": 0,
+              "h2smax": 6000,
+              "nh3min": 0,
+              "nh3max": 1500,
+              "tempmin": 0,
+              "tempmax": 30,
+              "humimin": 0,
+              "humimax": 40,
+              "luxmin": 0,
+              "luxmax": 100
+            }
+          },
+          "targets": {
+            "0": ["B54E453C"]
+          },
+          "actions": {
+            "0": {
+              "light": "open",
+              "fun": "open"
+            }
+          }
+        }
+      },
+      ruleNextKey: null,
+      source: '',
+      condition: {
+        "co2min": 400,
+        "co2max": 60000,
+        "comin": 10,
+        "comax": 1000,
+        "h2smin": 1,
+        "h2smax": 200,
+        "nh3min": 5,
+        "nh3max": 500,
+        "tempmin": 0,
+        "tempmax": 50,
+        "humimin": 20,
+        "humimax": 90,
+        "luxmin": 0,
+        "luxmax": 65535
+      },
+      target: '',
+      action: {
+        'light': '',
+        'fun': '',
+      },
+      co: [10, 1000],
+      co2: [400, 60000],
+      h2s: [1, 200],
+      lux: [0, 65535],
+      nh3: [5, 500],
+      humi: [20, 90],
+      temp: [0, 50],
+      light: false,
+      fun: false,
+      hasJsonFlag: true,  // json是否验证通过
+      // json数据
+      resultInfo: {
+        'employees': [
+          {
+            'firstName': 'Bill',
+            'lastName': 'Gates'
+          },
+          {
+            'firstName': 'George',
+            'lastName': 'Bush'
+          },
+          {
+            'firstName': 'Thomas',
+            'lastName': 'Carter'
+          }
+        ]
+      }
+    }
+  },
+  mounted: function () {
+    this.ruleNextKey = this.findMaxKey(this.rules)
+  },
+  methods: {
+    onJsonChange(value) {
+      // console.log('更改value:', value);
+      // 实时保存
+      this.onJsonSave(value)
+    },
+    onJsonSave(value) {
+      // console.log('保存value:', value);
+      this.rules = value
+      this.hasJsonFlag = true
+    },
+    onError(value) {
+      // console.log("json错误了value:", value);
+      this.hasJsonFlag = false
+    },
+    // 检查json
+    checkJson() {
+      if (this.hasJsonFlag == false) {
+        // console.log("json验证失败")
+        // this.$message.error("json验证失败")
+        alert("json验证失败")
+        return false
+      } else {
+        // console.log("json验证成功")
+        // this.$message.success("json验证成功")
+        alert("json验证成功")
+        return true
+      }
+    },
+
+    // 找最大的键，返回下一个键
+    findMaxKey(obj) {
+      let maxKey = -Infinity;
+      if (!obj.hasOwnProperty('0')) {
+        return String(0)
+      }
+      for (let key in obj) {
+        if (parseInt(key) > maxKey) {
+          maxKey = key;
+        }
+      }
+      maxKey += 1
+      if (maxKey < 10) {
+        return String(maxKey).slice(1)
+      }
+      return String(maxKey)
+    },
+
+    confirmSource() {
+      const regex = /^[0-9a-fA-F]{8}$/;
+      console.log(this.rules)
+      console.log(this.source)
+      if (regex.test(this.source)) {
+        if (!this.rules.hasOwnProperty(this.ruleNextKey)) {
+          this.rules[this.ruleNextKey] = { 'source': this.source }
+        }
+        this.$message({
+          message: '已设置数据源',
+          type: 'success'
+        });
+        return
+      }
+      this.$message.error('DevAddr格式错误');
+      this.source = ''
+    },
+
+    appendTarget() {
+      const regex = /^[0-9a-fA-F]{8}$/;
+      if (regex.test(this.target)) {
+        if (!this.rules[this.ruleNextKey].hasOwnProperty('targets')) {
+          Object.assign(this.rules[this.ruleNextKey],{ 'targets': [] })
+        }
+        this.rules[this.ruleNextKey].targets.push(this.target)
+        this.$message({
+          message: '已追加目标',
+          type: 'success'
+        });
+        console.log(this.rules)
+        return
+      }
+      this.$message.error('DevAddr格式错误');
+      this.target = ''
+    },
+
+    appendCondition() {
+      if (!this.rules[this.ruleNextKey].hasOwnProperty('conditions')) {
+        Object.assign(this.rules[this.ruleNextKey],{ 'conditions': {} })
+      }
+      this.condition.comin = this.co[0]
+      this.condition.comax = this.co[1]
+      this.condition.co2min = this.co2[0]
+      this.condition.co2max = this.co2[1]
+      this.condition.h2smin = this.h2s[0]
+      this.condition.h2smax = this.h2s[1]
+      this.condition.luxmin = this.lux[0]
+      this.condition.luxmax = this.lux[1]
+      this.condition.nh3min = this.nh3[0]
+      this.condition.nh3max = this.nh3[1]
+      this.condition.humimin = this.humi[0]
+      this.condition.humimax = this.humi[1]
+      this.condition.tempmin = this.temp[0]
+      this.condition.tempmax = this.temp[1]
+      var nextConditionKey = this.findMaxKey(this.rules[this.ruleNextKey].conditions)
+      let cond = {}
+      cond[nextConditionKey] = this.condition
+      Object.assign(this.rules[this.ruleNextKey].conditions, cond)
+      this.$message({
+        message: '已追加条件组',
+        type: 'success'
+      });
+    },
+
+    appendAction() {
+      if (!this.rules[this.ruleNextKey].hasOwnProperty('actions')) {
+        Object.assign(this.rules[this.ruleNextKey],{ 'actions': {} })
+      }
+      if (typeof this.light === 'undefined' || typeof this.fun === 'undefined') {
+        this.$message.error('动作组未初始化');
+      }
+      if (this.light) {
+        this.action.light = 'open'
+      } else {
+        this.action.light = 'close'
+      }
+      if (this.fun) {
+        this.action.fun = 'open'
+      } else {
+        this.action.fun = 'close'
+      }
+      var nextActKey = this.findMaxKey(this.rules[this.ruleNextKey].actions)
+      let act = {}
+      act[nextActKey] = this.action
+      Object.assign(this.rules[this.ruleNextKey].actions, act)
+      console.log(this.rules)
+      this.$message({
+        message: '已追加动作组',
+        type: 'success'
+      });
+    },
+
+    appendRule() {
+      this.onJsonSave()
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-.rule-op-container {
+.rules-op-container {
   display: flex;
   // justify-content: space-between;
   align-items: center;
+  height: 10vh;
+}
+
+.rule-editor-container {
+  display: flex;
+  height: 80vh;
+  width: 100vw;
 }
 
 .json-edit-container {
-  height: 95vh;
-  width: 50vw;
+  height: 80vh;
+  width: 30vw;
+}
+
+.json-editor {
+  height: 100%;
+  width: 100%;
+}
+
+.jsoneditor-vue {
+  height: 80vh;
+}
+
+.jsoneditor-poweredBy {
+  display: none;
+}
+
+.rule-op-container {
+  height: 80vh;
+  width: 65vw;
+  // display: flex;
+  // flex-direction: column;
+}
+
+.rule-source-container,
+.rule-targets-container {
+  display: flex;
+  justify-content: center;
+  padding: 3px;
+}
+
+.el-input {
+  width: 15vw;
+}
+
+.el-button-source,
+.el-button-targets {}
+
+.rule-condition-container {
+  padding: 5px;
+}
+
+.condition-co,
+.condition-co2,
+.condition-h2s,
+.condition-lux,
+.condition-nh3,
+.condition-humi,
+.condition-temp {
+  width: 60vw;
+  padding: 1px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.el-slider {
+  width: 60vw;
+  margin-left: 15px;
+  margin-right: 35px;
+}
+
+.slider-text {
+  margin-left: 1vw;
+  width: 15vw;
+  display: inline-block;
+  /* 设置为 inline-block 块级元素 */
+  white-space: nowrap;
+  /* 禁止换行 */
+}
+
+.condition-append-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.rule-actions-container {
+  padding: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.switch-text {
+  margin-right: 10px;
+}
+
+.el-switch {
+  padding: 10px;
 }
 </style>
